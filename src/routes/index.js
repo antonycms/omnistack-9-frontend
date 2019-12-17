c58949920737b2f812e2ad4389e4393fe96039de
Antony Carlos <antonyc828@gmail.com>
2019-12-17 05:35:39 -0300

    added routes navigation

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from '../pages/Login'
+import Home from '../pages/Home'
+import Register from '../pages/Register'
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={Login} />
+      <Route path="/home" component={Home} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  );
+}
