e44044e433a7e78cb23121ff503bca793ff4f44d
Antony Carlos <antonyc828@gmail.com>
2019-12-18 16:20:05 -0300

    added dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import api from '../../services/api';
 
+import './Dashboard.css';
 import logo from '../../assets/logo.svg';
+import spotExample from '../../assets/spot-example.jpeg'
 
 export default function Dashboard() {
 
@@ -22,11 +24,23 @@ export default function Dashboard() {
     <div className="login-container">
       <img src={logo} alt="logo" />
       <form action="">
-        <p>
-          Ofereca <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-          </p>
-        <label htmlFor="email">E-MAIL *</label>
-        <input id="email" type="text" placeholder="Seu melhor e-mail" />
+        <p>Ofereca <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa</p>
+        <div id="btn-container">
+          <a href="/" style={{ "color": "green" }}>Aceitar</a>
+          <a href="/" style={{ "color": "red" }}>Recusar</a>
+        </div>
+        <div id="container-spots">
+          <div id="spot-left">
+            <img src={spotExample} alt="" />
+            <h6>Rocketseat</h6>
+            <p>R$ 0,00</p>
+          </div>
+          <div id="spot-right">
+            <img src={spotExample} alt="" />
+            <h6>Dev City</h6>
+            <p>R$ 20,00</p>
+          </div>
+        </div>
         <button type="submit">cadastrar</button>
       </form>
     </div>
